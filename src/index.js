bcb33ed372569fee4376ff70790ac71b4e6cf2ec
carlos-kennedy <carlosjustcarlos@outlook.com>
2024-09-10 16:46:12 -0300

    :memo: fundamentals 002

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 let meta = {
   value: "Ir para a acadêmia",
   checked: false,
-  log: (info) => {
-    console.log("Sou um método pois estou associado a um objeto");
-    console.log(info);
-  },
 };
 
-// metodo ou função
-const criarMeta = () => {
-  console.log("Sou uma função em arrow function usada para criar meta");
-};
-criarMeta();
-// Sou um método pois estou associado a um objeto
-meta.log(meta.value);
+let metas = [
+  meta,
+  {
+    value: "Estudar por mais de 1h",
+    checked: true,
+  },
+];
+
+console.log(metas[1].value);
