c965a6dcbe8f8937981cb41e1067bf1a915548c9
carlos-kennedy <carlosjustcarlos@outlook.com>
2024-09-10 20:12:28 -0300

    :construction: build: Adding menu to the application

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-while (true) {
-  let option = "Cadastrar";
-  switch (option) {
-    case "Cadastrar":
-      console.log("Vamos cadastrar");
-      break;
-    case "Listar":
-      console.log("Vamos listar");
-      break;
-    case "Sair":
-      return;
+import { select } from "@inquirer/prompts";
+
+const start = async () => {
+  while (true) {
+    // Aguardar ( Espere que o usuário vai selecionar alguma coisa)
+    const option = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar Meta",
+          value: "Cadastrar",
+        },
+        {
+          name: "Listar Metas",
+          value: "Listar",
+        },
+        {
+          name: "Sair",
+          value: "Sair",
+        },
+      ],
+    });
+    switch (option) {
+      case "Cadastrar":
+        console.log("Vamos cadastrar");
+        break;
+      case "Listar":
+        console.log("Vamos listar");
+        break;
+      case "Sair":
+        console.log("Volte sempre ! ");
+        return;
+    }
   }
-}
+};
+
+start();
